6475f68d5a495dd681eadf61abf0ad8711e1c181
vivekS <vrsolanki2004@gmail.com>
2026-07-01 10:31:12 +0530

    Configured project for deployment

--- student_management/settings.py
+++ student_management/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qj&nsw=%o=256c2*1xecf3*erw+%s0259p=64$r%wf_(6zpn21'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
